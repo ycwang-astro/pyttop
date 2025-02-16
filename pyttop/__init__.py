5713aa47d2ecdcf3932e712a97c7fde390f894e6
ycwang-astro <ycwang-astro@users.noreply.github.com>
2025-02-16 21:06:12 +0800

    increase version number

--- pyttop/__init__.py
+++ pyttop/__init__.py
@@ -22,6 +22,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.4.2-dev'
+__version__ = '0.4.2'
 
 from .sampledata import get_example
